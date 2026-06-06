9a63c6173d0cb5ba1aa3b3739871d1fd1310267e
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-06 20:38:02 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,48 @@
 import styles from './index.module.css';
-
 import Link from 'next/link';
 import products from '../products.json';
 import links from '../link.json';
 
-
 export default function Home() {
   return (
-  <div style={{ textAlign: 'center', padding: '20px' }}>
-    {/* جزء الشعار */}
-    <img src="/Logo.png" alt="My Store Logo" style={{ width: '150px', borderRadius: '50%' }} />
-    <h1>9smart</h1>
-     </div>
-    {/* عرض المنتجات */}
-    <div>
-      {products.map(product => (
-        <div key={product.slug}>
-          <Link href={`/product/${product.slug}`}>
-            {product.name}
-          </Link>
-        </div>
+    <>
+      <div style={{ textAlign: 'center', padding: '20px' }}>
+        <img
+          src="/Logo.png"
+          alt="My Store Logo"
+          style={{ width: '150px', borderRadius: '50%' }}
+        />
+        <h1>9smart</h1>
+      </div>
 
-      )                   
-      </div>   
+      {/* عرض المنتجات */}
+      <div>
+        {products.map(product => (
+          <div key={product.slug}>
+            <Link href={`/product/${product.slug}`}>
+              {product.name}
+            </Link>
+          </div>
+        ))}
+      </div>
 
+      {/* قسم الروابط */}
+      <div className={styles.linksSection}>
+        <h2>مواقع مفيدة</h2>
 
-
-    {/* هنا يمكنك إضافة قسم الروابط */}
-
-<div className={styles.linksSection}>
-  <h2>مواقع مفيدة</h2>
-  {links.map(link => (
-    <div key={link.url}>
-      <a href={link.url} target="_blank" rel="noopener noreferrer">
-        {link.title}
-      </a>
-      <p>{link.description}</p>
-    </div>
-     </div>  
-) }
-) }
-
-
-
+        {links.map(link => (
+          <div key={link.url}>
+            <a
+              href={link.url}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {link.title}
+            </a>
+            <p>{link.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
