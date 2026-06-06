e86bab6ad3143e264d2803766cbe77434767b28f
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-06 23:35:02 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,37 @@ import products from '../products.json';
 
 export default function Home() {
   return (
-    <>
-      <div style={{ textAlign: 'center', padding: '20px' }}>
-        <img
-          src="/Logo.png"
-          alt="My Store Logo"
-          style={{ width: '150px', borderRadius: '50%' }}
-        />
-        <h1>9smart</h1>
-      </div>
+  <div style={{ textAlign: 'center', padding: '20px' }}>
+    {/* جزء الشعار */}
+    <img src="/Logo.PNG" alt="My Store Logo" style={{ width: '150px', borderRadius: '50%' }} />
+    <h1>Welcome to Our Store</h1>
+    
+    {/* عرض المنتجات */}
+    <div>
+      {products.map(product => (
+        <div key={product.slug}>
+          <Link href={`/product/${product.slug}`}>
+            {product.name}
+          </Link>
+        </div>
+      ))}
+    </div>
 
-      {/* عرض المنتجات */}
-      <div>
-        {products.map((product) => (
-          <div key={product.slug}>
-            <Link href={`/product/${product.slug}`}>
-              {product.name}
-            </Link>
-          </div>
-        ))}
-      </div>
+    {/* هنا يمكنك إضافة قسم الروابط */}
+    <div style={{ marginTop: '40px' }}>
+      <h2>مواقع مفيدة</h2>
+      {links.map(link => (
+        <div key={link.url} style={{ marginBottom: '15px' }}>
+          <a href={link.url} target="_blank" rel="noopener noreferrer">
+            {link.title}
+          </a>
+          <p>{link.description}</p>
+        </div>
+      ))}
+    </div>
+  </div>
+);
+
     </>
   );
 }
