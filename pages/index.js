3940e598f1f1b598f9ed2bcd1e27ea7683ba15c1
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-07 21:05:39 +0300

    Fix import path for index.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import styles from 'index.module.css';
+import styles from './index.module.css';
 import Link from 'next/link';
 import products from '../products.json';
 import links from '../link.json';
