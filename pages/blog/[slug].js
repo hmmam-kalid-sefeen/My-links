b82b722144e3053847cf953fb83d44a4a23ba560
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-10 20:30:49 +0300

    Refactor PostPage component for styling and structure

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,25 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import ReactMarkdown from 'react-markdown';
+import styles from './post.module.css';
 
 export default function PostPage({ post }) {
   return (
-    <div style={{ padding: '20px', maxWidth: '800px', margin: 'auto' }}>
-      <h1 style={{ marginBottom: '20px' }}>{post.title}</h1>
-      <ReactMarkdown
-        components={{
-          h1: ({node, ...props}) => <h1 style={{color: '#2c3e50'}} {...props} />,
-          h2: ({node, ...props}) => <h2 style={{color: '#34495e', marginTop: '20px'}} {...props} />,
-          p: ({node, ...props}) => <p style={{lineHeight: '1.6', marginBottom: '15px'}} {...props} />,
-          img: ({node, ...props}) => (
-            <img style={{maxWidth: '100%', margin: '20px 0'}} {...props} />
-          ),
-          ul: ({node, ...props}) => <ul style={{paddingLeft: '20px', marginBottom: '15px'}} {...props} />,
-          li: ({node, ...props}) => <li style={{marginBottom: '8px'}} {...props} />
-        }}
-      >
-        {post.content}
-      </ReactMarkdown>
+    <div className={styles.container}>
+      <h1>{post.title}</h1>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </div>
   );
 }
