a19707b2e10bb0fc94426e2a8a5530c3b8b7b554
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-13 20:31:35 +0300

    Add Footer component with copyright information

---
+++ components/footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ background: '#0f172a', color: '#fff', padding: '40px', textAlign: 'center' }}>
+      <p>© 2026 9smart. All rights reserved.</p>
+    </footer>
+  );
+}
