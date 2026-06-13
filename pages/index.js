b371c50ee78d9e43789e50287092de2b8ec45aaa
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-13 20:54:25 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import fs from 'fs';
+import path from 'path';
 import Navbar from '../components/navbar';
 import Hero from '../components/hero';
 import CategoryCard from '../components/categorycard';
 import ArticleCard from '../components/articlecard';
 import Footer from '../components/footer';
 
-export default function Home({ posts }) {
+export default function Home({ posts = [] }) { // أضفنا = [] لحماية الموقع من الانهيار إذا كانت البيانات فارغة
   return (
     <>
       <Navbar />
@@ -19,4 +21,22 @@ export default function Home({ posts }) {
       <Footer />
     </>
   );
+}
+
+// دالة جلب البيانات من مجلد posts
+export async function getStaticProps() {
+  const postsDirectory = path.join(process.cwd(), 'posts');
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const posts = filenames.map(filename => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(fileContents);
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
