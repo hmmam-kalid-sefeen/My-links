c68a617f5180a759f3363ca33141ba1498bfe070
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-13 20:28:41 +0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,1 +1,12 @@
+import Link from 'next/link';
 
+export default function Navbar() {
+  return (
+    <nav style={{ padding: '20px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+      <div style={{ fontWeight: 'bold', fontSize: '20px' }}>9smart</div>
+      <div>
+        <Link href="/">Home</Link> | <Link href="/blog">Blog</Link>
+      </div>
+    </nav>
+  );
+}
