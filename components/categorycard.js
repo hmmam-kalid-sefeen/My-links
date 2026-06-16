7d8416227d51aedb7d030f8d79517a6dd2d0f201
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-17 01:07:19 +0300

    Use default image if none provided in CategoryCard

--- components/categorycard.js
+++ components/categorycard.js
@@ -1,8 +1,10 @@
 export default function CategoryCard({ title, icon }) {
   return (
     <div className="card" style={{ padding: '30px', border: '1px solid #eee', borderRadius: '15px', textAlign: 'center' }}>
-      <img src={image} alt={title} style={{ width: '50px', height: '50px' }} />
+<img src={image || '/default-placeholder.jpg'} alt={title} />
       <h3>{title}</h3>
     </div>
   );
 }
+
+
