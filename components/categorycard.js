1b5e92287c6609b280723197ce4d16e0981a5b4a
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-17 00:39:14 +0300

    Update categorycard.js

--- components/categorycard.js
+++ components/categorycard.js
@@ -1,7 +1,7 @@
 export default function CategoryCard({ title, icon }) {
   return (
     <div className="card" style={{ padding: '30px', border: '1px solid #eee', borderRadius: '15px', textAlign: 'center' }}>
-      <div>{icon}</div>
+      <img src={image} alt={title} style={{ width: '50px', height: '50px' }} />
       <h3>{title}</h3>
     </div>
   );
