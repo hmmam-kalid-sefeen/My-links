fb27030e1be19b041f021853c0da66aa93d3c71a
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-18 19:32:01 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -28,11 +28,11 @@ export default function Home({ posts = [] }) {
 <div className={styles.categoriesGrid}>
   <CategoryCard 
     title="Top Gadgets" 
-    image="/gadget-image.jpg" // تأكد أن الصورة بهذا الاسم في مجلد public
+    image="/gadget.jpg" // تأكد أن الصورة بهذا الاسم في مجلد public
   />
   <CategoryCard 
     title="Essential Software" 
-    image="/software-image.jpg" 
+    image="/software.jpg" 
   />
 </div>
 
