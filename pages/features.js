7d7e5ebcbd764e734794426582d40ee41c976b97
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-18 21:51:15 +0300

    Update features.js

--- pages/features.js
+++ pages/features.js
@@ -1,1 +1,16 @@
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
+export default function Features() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: '50px', textAlign: 'center' }}>
+        <h1>المميزات (Features)</h1>
+        <p>هنا يمكنك إضافة قائمة بمميزات أدوات التقنية والبرمجيات التي تتحدث عنها.</p>
+        {/* يمكنك إضافة مكونات أخرى هنا */}
+      </main>
+      <Footer />
+    </>
+  );
+}
