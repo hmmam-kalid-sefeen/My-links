fff7419fdd3ea1f2fcd1cc74a317c73fbc263960
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-18 21:55:52 +0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -1,19 +1,15 @@
 import Link from 'next/link';
 import styles from '../pages/navbar.module.css';
 
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <div className={styles.logo}>9smart</div>
-      
-      <div className={styles.links}>
-        {/* حذفنا الأسهم ▾ من هنا */}
-        <Link href="#">Features</Link> 
-        <Link href="#">About</Link>
-        <Link href="/blog">Blog</Link>
-      </div>
-
-      {/* الزر تم حذفه أو إخفاؤه بـ CSS */}
+    <nav className="nav-container">
+      <Link href="/">9smart</Link>
+      <Link href="/features">Features</Link>
+      <Link href="/about">About</Link>
+      <Link href="/blog">Blog</Link>
     </nav>
   );
 }
