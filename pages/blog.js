5091315cbf2441d52b7a986976ba6ff5fb72db9c
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-18 22:12:51 +0300

    Add Navbar and Footer to Blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,1 +1,15 @@
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
+export default function Blog() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: '80px 20px', textAlign: 'center' }}>
+        <h1>Our Blog</h1>
+        <p>Stay tuned! Our articles are coming soon.</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
