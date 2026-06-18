51910b8f496f28265e183d6ca20c03d44d2557e8
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-19 00:35:54 +0300

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -2,18 +2,11 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '30px', padding: '20px' }}>
-      
-      {/* استبدلنا النص هنا بالصورة */}
+    <nav style={{ display: 'flex', justifyContent: 'center', gap: '30px', padding: '20px' }}>
       <Link href="/">
-        <img 
-          src="/Logo.png" 
-          alt="9smart Logo" 
-          style={{ height: '90px', cursor: 'pointer' }} 
-        />
+        <img src="/logo.png" alt="Logo" style={{ height: '40px' }} />
       </Link>
-
-      <Link href="/contact" style={{ color: '#ffffff', textDecoration: 'none', fontWeight: 'bold' }}Contact Us</Link>
+      <Link href="/contact" style={{ color: '#ffffff', textDecoration: 'none', fontWeight: 'bold' }}>Contact Us</Link>
       <Link href="/about" style={{ color: '#ffffff', textDecoration: 'none', fontWeight: 'bold' }}>About</Link>
       <Link href="/blog" style={{ color: '#ffffff', textDecoration: 'none', fontWeight: 'bold' }}>Blog</Link>
     </nav>
