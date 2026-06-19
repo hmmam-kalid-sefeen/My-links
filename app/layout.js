1ac0b16e1d29f3e4ee5b0cc3894ff484b40b405b
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-19 21:08:20 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,17 @@
+// app/layout.js
+import Navbar from '../components/navbar';
+import Footer from '../components/footer';
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ar" dir="rtl">
+      <body>
+        <Navbar />
+        <main style={{ marginTop: '80px' }}>
+          {children} {/* هنا ستظهر كل صفحاتك تلقائياً */}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
