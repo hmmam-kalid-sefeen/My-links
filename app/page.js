cf065bc69f02fa5af46837500265318448d74e8d
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-20 01:01:22 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import Hero from '../components/hero';
 import CategoryCard from '../components/categorycard';
 import ArticleCard from '../components/articlecard';
-import styles from '../components/home.module.css';
+import styles from '../home.module.css';
 import fs from 'fs';
 import path from 'path';
 
