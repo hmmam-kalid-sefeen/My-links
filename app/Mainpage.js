3297d3944d112e5ee7046d5d66380d8dab752179
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-19 21:18:18 +0300

    Add Footer component to Mainpage

--- app/Mainpage.js
+++ app/Mainpage.js
@@ -1,1 +1,16 @@
 
+import Footer from '../components/footer';
+
+export default function Blog() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ padding: '80px 20px' }}>
+
+        <p>home</p> 
+        <p>articles are coming soon.</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
