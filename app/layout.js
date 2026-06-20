eceb2bab543b5451bfa323ef2b6ed3b800a29609
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-20 20:29:04 +0300

    Update import paths for Navbar and Footer components

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 // app/layout.js
-import Navbar from '../components/navbar';
-import Footer from '../components/footer';
+import Navbar from '../comps/navbar';
+import Footer from '../comps/footer';
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
