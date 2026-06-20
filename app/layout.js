a9032ca61f4fddc95274cb14026f46adfc9a6b45
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-20 21:50:59 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,15 @@
 // app/layout.js
-import Navbar from '../comps/navbar';
+import './globals.css'; // بما أن الملف أصبح الآن داخل app، هذا المسار صحيح
+import Navbar from '../comps/navbar'; 
 import Footer from '../comps/footer';
+
 export default function RootLayout({ children }) {
   return (
     <html lang="ar" dir="rtl">
       <body>
         <Navbar />
-        <main style={{ marginTop: '80px' }}>
-          {children} {/* هنا ستظهر كل صفحاتك تلقائياً */}
+        <main>
+          {children}
         </main>
         <Footer />
       </body>
