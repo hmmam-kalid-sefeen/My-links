9e5575e5d622368472ebd9caeaf2ce3df3b5eb14
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-23 00:02:56 +0300

    Delete app/essential-software directory

--- app/essential-software/page.js
+++
@@ -1,8 +0,0 @@
-export default function EssentialSoftwarePage() {
-  return (
-    <main style={{ padding: '20px' }}>
-      <h1>Essential Software</h1>
-      <p>welcome to softwares page.</p>
-    </main>
-  );
-}
