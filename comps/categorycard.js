e09b2331edecfbb5f97074e2fc96c52cee038a9a
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-27 17:27:38 +0300

    Increase image height from 100px to 120px

--- comps/categorycard.js
+++ comps/categorycard.js
@@ -20,7 +20,7 @@ export default function CategoryCard({ title, image }) {
           alt={title} 
           style={{ 
             width: '100%', 
-            height: '100px', 
+            height: '120px', 
             borderRadius: '10px', 
             objectFit: 'cover' // هذا هو السطر الذي يوحد مقياس الصور ويمنع تشوهها
           }} 
