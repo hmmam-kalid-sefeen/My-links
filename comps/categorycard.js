2da041c69ac4997b3ca7897a195e7bc558db8724
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-06-27 16:04:20 +0300

    Update categorycard.js

--- comps/categorycard.js
+++ comps/categorycard.js
@@ -8,7 +8,7 @@ const linkPath = `/category/${title.toLowerCase().replace(' ', '-')}`;
   return (
     <Link href={linkPath} style={{ textDecoration: 'none', color: 'inherit' }}>
       <div style={{ textAlign: 'center', padding: '15px', border: '1px solid #e5e7eb', borderRadius: '15px', cursor: 'pointer', backgroundColor: 'white' }}>
-        <img src={image} alt={title} style={{ width: '100%', borderRadius: '10px', height: 'auto' }} />
+        <img src={image} alt={title} style={{ width: '100%', borderRadius: '10px', height: '200px' }} />
         <h3 style={{ marginTop: '15px', marginBottom: '5px' }}>{title}</h3>
       </div>
     </Link>
