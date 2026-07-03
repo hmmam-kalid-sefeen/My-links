97856961ad20f765f5425420731b526c117b5851
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-03 06:13:01 +0300

    Update AdBanner.js

--- comps/AdBanner.js
+++ comps/AdBanner.js
@@ -1,5 +1,24 @@
-export default function AdBanner() {
+'use client'; // ضروري لأن الإعلان يتفاعل مع المتصفح
+
+import { useEffect, useRef } from 'react';
+
+export default function AdsterraAd() {
+  const adRef = useRef(null);
+
+  useEffect(() => {
+    // التأكد من عدم تكرار إضافة السكربت إذا تم تحميل المكون أكثر من مرة
+    if (adRef.current && !adRef.current.firstChild) {
+      const script = document.createElement('script');
+      script.async = true;
+      script.setAttribute('data-cfasync', 'false');
+      script.src = "https://pl30178671.effectivecpmnetwork.com/b4827f224d08a07b93dc25b2a58a8ff5/invoke.js";
+      adRef.current.appendChild(script);
+    }
+  }, []);
+
   return (
-    <div dangerouslySetInnerHTML={{ __html: `كود الإعلان الذي نسخته هنا` }} />
+    <div ref={adRef}>
+      <div id="container-b4827f224d08a07b93dc25b2a58a8ff5"></div>
+    </div>
   );
 }
