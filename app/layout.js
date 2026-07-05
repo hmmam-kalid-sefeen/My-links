f566d18755097e47d47c99b47fe2999f38c7a3d3
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-05 05:41:40 +0300

    Integrate AdBanner component into layout
    Added AdBanner component for advertisement display.

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,16 @@
 // app/layout.js
-import './globals.css'; // بما أن الملف أصبح الآن داخل app، هذا المسار صحيح
+import './globals.css'; // بما أن الملف داخل app، هذا المسار صحيح
 import Navbar from '../comps/navbar'; 
 import Footer from '../comps/footer';
+import AdBanner from '../comps/AdBanner'; // استدعاء مكون الإعلان
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
+        {/* مكون الإعلان هنا ليعمل في كل الصفحات */}
+        <AdBanner /> 
         <main>
           {children}
         </main>
