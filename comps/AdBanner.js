77794c6810ef15cfc438d73d38384504646c34a0
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-07 05:47:42 +0300

    Update AdBanner.js

--- comps/AdBanner.js
+++ comps/AdBanner.js
@@ -1,10 +1,2 @@
-<script>
-  atOptions = {
-    'key' : '641c4b7a0004c647a3fb82de25ff7519',
-    'format' : 'iframe',
-    'height' : 250,
-    'width' : 300,
-    'params' : {}
-  };
-</script>
-<script src="https://www.highperformanceformat.com/641c4b7a0004c647a3fb82de25ff7519/invoke.js"></script>
+<script async="async" data-cfasync="false" src="https://pl30178671.effectivecpmnetwork.com/b4827f224d08a07b93dc25b2a58a8ff5/invoke.js"></script>
+<div id="container-b4827f224d08a07b93dc25b2a58a8ff5"></div>
