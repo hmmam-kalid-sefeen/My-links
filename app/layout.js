66d0239a476add06ab346317609f44dbf8daebe6
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-07 05:59:19 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css';
 import Navbar from '../comps/navbar'; 
 import Footer from '../comps/footer';
 // اترك سطر الاستيراد معلقاً هكذا
-// import AdBanner from '../comps/AdBanner'; 
+import AdBanner from '../comps/AdBanner'; 
 
 export default function RootLayout({ children }) {
   return (
@@ -10,7 +10,8 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         {/* تعليق المكون يضمن أنه لن يتم استدعاؤه في الصفحة */}
-        {/* <AdBanner /> */}
+    //   ddd {/* <AdBanner /> */}
+          <AdBanner /> 
         <main>
           {children}
         </main>
