943656cb16061f7301afb0df91132f4d195c6079
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-07 05:36:57 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@
 import './globals.css'; // بما أن الملف داخل app، هذا المسار صحيح
 import Navbar from '../comps/navbar'; 
 import Footer from '../comps/footer';
-//import AdBanner from '../comps/AdBanner'; // استدعاء مكون الإعلان
+import AdBanner from '../comps/AdBanner'; // استدعاء مكون الإعلان
 
 export default function RootLayout({ children }) {
   return (
@@ -10,7 +10,10 @@ export default function RootLayout({ children }) {
       <body>
         <Navbar />
         {/* مكون الإعلان هنا ليعمل في كل الصفحات */}
-    //    <AdBanner /> 
+     // بدلاً من حذف الكود، أضف هذا الشرط
+{false && <AdBanner />}
+
+
         <main>
           {children}
         </main>
