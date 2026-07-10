610df7eeb4638fdbcaddacc6f09bf07aeadc31cf
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-11 00:00:34 +0300

    Remove TypeScript ignore build errors comment
    Removed comment about ignoring TypeScript errors during build.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // هذا السطر يخبر Vercel بتجاهل أخطاء TypeScript أثناء البناء
     ignoreBuildErrors: true,
   },
   async headers() {
