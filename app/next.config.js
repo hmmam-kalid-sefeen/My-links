dc0504558a7bd598b750d025f60f3c4bface36fb
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-10 05:53:05 +0300

    Delete app/next.config.js

--- app/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    // هذا السطر سيتجاهل أخطاء الـ TypeScript أثناء عملية البناء
-    ignoreBuildErrors: true,
-  },
-}
-
-module.exports = nextConfig
