ffa266d71f02a4b0654107e123f3fa41b45d178c
hmmam-kalid-sefeen <124155148+hmmam-kalid-sefeen@users.noreply.github.com>
2026-07-11 00:10:37 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async headers() {
-    return [
-      {
-        source: '/sitemap.xml',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/xml',
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
